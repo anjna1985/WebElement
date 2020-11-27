4d3ff662790eeb7fcced8c0585e7aa03a8ecbd01
14012 <14012@LAPTOP-0G4AGPL8.home>
2020-11-27 06:28:21 -0500

    1st Commit

---
+++ src/test/java/sampleTestNG/SampleTestNG.java
@@ -0,0 +1,34 @@
+package sampleTestNG;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTestNG {
+
+	public String baseUrl = "http://demo.guru99.com/test/newtours/";
+	String driverPath = "C:\\Users\\14012\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe";
+	public WebDriver driver;
+
+	@Test
+	public void verifyHomepageTitle() {
+
+		System.out.println("launching chrome browser");
+		System.setProperty("webdriver.chrome.driver",driverPath);
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().deleteAllCookies();
+		driver.get(baseUrl);
+		String expectedTitle = "Welcome: Mercury Tours";
+		String actualTitle = driver.getTitle();
+		AssertJUnit.assertEquals(actualTitle, expectedTitle);
+		//driver.close();
+	}
+
+}
