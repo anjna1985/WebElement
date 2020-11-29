83314071dd7ffa0fd764cc0d932e06114f089123
Bharat <anjna85@gmail.com>
2020-11-29 16:34:42 -0500

    final commit

--- src/test/java/sampleTestNG/SampleTest.java
+++ src/test/java/sampleTestNG/SampleTest.java
@@ -20,7 +20,7 @@ public class SampleTest {
 	public void verifyHomepageTitle() {
 
 		System.out.println("launching chrome browser");
-		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"src\\main\\resources\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\main\\resources\\drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
